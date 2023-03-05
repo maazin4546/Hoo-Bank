6271323b4f67ead6919b99a5b0e949067980d991
Maazin Abdullah <mazinabdullah4546@gmail.com>
2023-03-05 13:04:52 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import './App.css'
+import styles from './style'
+import {
+  Navbar, Billing, Business, Button, CardDeal, Clients, CTA, FeedBackcard, Footer, GetStarted, Hero, Stats, Testimonials
+} from './components'
+
+function App() {
+
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
